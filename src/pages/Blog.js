ea97f2302c38b479b5511efecd6f07cf1983d3f1
Kedus Leji Yared <kd10yared237@gmail.com>
2022-07-27 16:47:12 +0300

    Fixed bugs

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link as RLink } from 'react-router-dom';
 import {
     Box,
     Heading,
@@ -17,14 +18,13 @@ import {
 } from '@chakra-ui/react';
 import client from "../client";
 import BlockContent from '@sanity/block-content-to-react';
-import axios from "axios";
 
 const BlogTags = (props) => {
     return (
         <HStack spacing={2} marginTop={props.marginTop}>
             {props.tags.map((tag) => {
                 return (
-                    <Tag size={'md'} variant="solid" bg="#47A166" key={tag}>
+                    <Tag key={Math.random() * Math.PI} size={'md'} variant="solid" bg="#47A166" key={tag}>
                         {tag}
                     </Tag>
                 );
@@ -34,12 +34,14 @@ const BlogTags = (props) => {
 };
 
 export const BlogAuthor = (props) => {
+    const imgUrl = props.authors && props.authors.filter(author => author._id === props.authorRef)[0].imageUrl;
     return (
         <HStack marginTop="2" spacing="2" display="flex" alignItems="center">
             <Image
                 borderRadius="full"
                 boxSize="40px"
-                src={props.image}
+                objectFit="cover"
+                src={imgUrl ? imgUrl : undefined}
                 alt={`Avatar of ${props.name}`}
             />
             <Text fontWeight="medium">{props.name}</Text>
@@ -52,37 +54,41 @@ export const BlogAuthor = (props) => {
 const BlogPost = (props) => {
     return (
         <WrapItem width={{ base: '100%', sm: '45%', md: '45%', lg: '30%' }}>
-            <Box w="100%">
-                <Box borderRadius="lg" overflow="hidden">
-                    <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
-                        <Image
-                            transform="scale(1.0)"
-                            src={props.image}
-                            alt={props.title}
-                            objectFit="contain"
-                            width="450px"
-                            transition="0.3s ease-in-out"
-                            _hover={{
-                                transform: 'scale(1.05)',
-                            }}
-                        />
-                    </Link>
+            <RLink to={props.slug}>
+                <Box w="100%">
+                    <Box borderRadius="lg" overflow="hidden">
+                        <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
+                            <Image
+                                transform="scale(1.0)"
+                                src={props.image}
+                                alt={props.title}
+                                objectFit="contain"
+                                width="450px"
+                                transition="0.3s ease-in-out"
+                                _hover={{
+                                    transform: 'scale(1.05)',
+                                }}
+                            />
+                        </Link>
+                    </Box>
+                    <BlogTags tags={['Engineering', 'Product']} marginTop="3" />
+                    <Heading fontSize="xl" marginTop="2">
+                        <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
+                            {props.title}
+                        </Link>
+                    </Heading>
+                    <Text as="p" fontSize="md" marginTop="2">
+                        <BlockContent blocks={!props.loading && props.body} projectId="82l3omkv" dataset="production" />
+                    </Text>
+                    <BlogAuthor
+                        name={props.author}
+                        authorRef={props.authorRef}
+                        authors={props.authors}
+                        loading={props.loading}
+                        date={new Date('2021-04-06T19:01:27Z')}
+                    />
                 </Box>
-                <BlogTags tags={['Engineering', 'Product']} marginTop="3" />
-                <Heading fontSize="xl" marginTop="2">
-                    <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
-                        {props.title}
-                    </Link>
-                </Heading>
-                <Text as="p" fontSize="md" marginTop="2">
-                    <BlockContent blocks={!props.loading && props.body} projectId="82l3omkv" dataset="production" />
-                </Text>
-                <BlogAuthor
-                    name={props.author}
-                    image={props.authorImage}
-                    date={new Date('2021-04-06T19:01:27Z')}
-                />
-            </Box>
+            </RLink>
         </WrapItem>
     );
 }
@@ -91,10 +97,10 @@ const BlogPost = (props) => {
 const Blog = () => {
     const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [author, setAuthor] = useState({});
+    const [authors, setAuthors] = useState({});
 
     useEffect(() => {
-        client.fetch(
+        const getPost = async () => await client.fetch(
             `*[_type == "post"] {
                     title,
                     slug,
@@ -111,86 +117,89 @@ const Blog = () => {
                         _ref
                     }
                 }`
-        ).then(data => { setPosts(data) }).catch(err => console.error(err));
-        client.fetch(`
+        ).then(data => { setPosts(data); setLoading(false); }).catch(err => console.error(err));
+        getPost();
+        const getAuthor = async () => await client.fetch(`
         *[_type == "author"] {
             "imageUrl": image.asset->url,
-            image {
-                asset -> {
-                    _ref
-                }
-            }
-        }`).then(data => console.log(data))
+            _id
+        }`).then(data => { setAuthors({ data }); setLoading(false) }).catch(err => console.error(err));
+        getAuthor();
     }, []);
-
-    console.log(!loading && posts[0]);
 
     return (
         <Container maxW={'8xl'} p="12">
             <Box p={5}>
                 <Heading as="h1" color="#47A166" pl={6}>Latest Blog</Heading>
-                <Box
-                    marginTop={{ base: '1', sm: '5' }}
-                    mb="12"
-                    display="flex"
-                    flexDirection={{ base: 'column', sm: 'row' }}
-                    justifyContent="space-between">
+                <RLink to={!loading && posts[0].slug.current}>
                     <Box
+                        marginTop={{ base: '1', sm: '5' }}
+                        mb="12"
                         display="flex"
-                        flex="1"
-                        marginRight="3"
-                        position="relative"
-                        alignItems="center">
+                        flexDirection={{ base: 'column', sm: 'row' }}
+                        justifyContent="space-between">
                         <Box
-                            width={{ base: '100%', sm: '85%' }}
-                            zIndex="2"
-                            marginLeft={{ base: '0', sm: '5%' }}
-                            marginTop="5%">
-                            <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
-                                <Image
-                                    borderRadius="lg"
-                                    maxWidth="30rem"
-                                    boxShadow="md"
-                                    src={!loading && posts[0].mainImage.asset.url}
-                                    alt={!loading && posts[0].title}
-                                    objectFit="contain"
-                                />
-                            </Link>
-                        </Box>
-                        <Box zIndex="1" width="100%" position="absolute" height="100%">
+                            display="flex"
+                            flex="1"
+                            marginRight="3"
+                            position="relative"
+                            alignItems="center">
                             <Box
-                                bgGradient={useColorModeValue(
-                                    'radial(green.500 1px, transparent 1px)',
-                                    'radial(green.500 1px, transparent 1px)'
-                                )}
-                                backgroundSize="20px 20px"
-                                opacity="1"
-                                height="100%"
+                                width={{ base: '100%', sm: '85%' }}
+                                zIndex="2"
+                                marginLeft={{ base: '0', sm: '5%' }}
+                                marginTop="5%">
+                                <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
+                                    <Image
+                                        borderRadius="lg"
+                                        maxWidth="30rem"
+                                        boxShadow="md"
+                                        src={!loading ? posts[0].mainImage.asset.url : undefined}
+                                        alt={!loading ? posts[0].title : "Blog post main cover"}
+                                        objectFit="contain"
+                                    />
+                                </Link>
+                            </Box>
+                            <Box zIndex="1" width="100%" position="absolute" height="100%">
+                                <Box
+                                    bgGradient={useColorModeValue(
+                                        'radial(green.500 1px, transparent 1px)',
+                                        'radial(green.500 1px, transparent 1px)'
+                                    )}
+                                    backgroundSize="20px 20px"
+                                    opacity="1"
+                                    height="100%"
+                                />
+                            </Box>
+                        </Box>
+                        <Box
+                            display="flex"
+                            flex="1"
+                            flexDirection="column"
+                            justifyContent="center"
+                            marginTop={{ base: '3', sm: '0' }}>
+                            <BlogTags tags={['Engineering', 'Product']} />
+                            <Heading marginTop="1">
+                                <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
+                                    {!loading && posts[0].title}
+                                </Link>
+                            </Heading>
+                            <Text
+                                as="p"
+                                marginTop="2"
+                                color={useColorModeValue('gray.700', 'gray.200')}
+                                fontSize="lg">
+                            </Text>
+                            <BlockContent blocks={!loading && posts[0].body} projectId="82l3omkv" dataset="production" />
+                            <BlogAuthor
+                                name={!loading && posts[0].authorName}
+                                date={new Date('2021-04-06T19:01:27Z')}
+                                authors={!loading && authors.data}
+                                authorRef={!loading && posts[0].author._ref}
                             />
                         </Box>
                     </Box>
-                    <Box
-                        display="flex"
-                        flex="1"
-                        flexDirection="column"
-                        justifyContent="center"
-                        marginTop={{ base: '3', sm: '0' }}>
-                        <BlogTags tags={['Engineering', 'Product']} />
-                        <Heading marginTop="1">
-                            <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
-                                {!loading && posts[0].title}
-                            </Link>
-                        </Heading>
-                        <Text
-                            as="p"
-                            marginTop="2"
-                            color={useColorModeValue('gray.700', 'gray.200')}
-                            fontSize="lg">
-                        </Text>
-                        <BlockContent blocks={!loading && posts[0].body} projectId="82l3omkv" dataset="production" />
-                        <BlogAuthor name="John Doe" date={new Date('2021-04-06T19:01:27Z')} />
-                    </Box>
-                </Box>
+                </RLink>
             </Box>
             {/* All Blogs */}
             <Heading as="h2" marginTop="5" color="#47A166">
@@ -198,11 +207,20 @@ const Blog = () => {
             </Heading>
             <Divider marginTop="5" />
             <Wrap spacing="30px" marginTop="5">
-                {posts.map(post => (
-                    <BlogPost key={post.slug.current} title={post.title} author={post.authorName} image={post.mainImage && post.mainImage.asset && post.mainImage.asset.url} body={post.body} authorImage={post.authorImage && post.authorImage.asset && post.authorImage.asset.url} loading={loading} />
+                {!loading && posts.map(post => (
+                    <BlogPost
+                        key={post.slug.current}
+                        slug={post.slug.current}
+                        title={post.title}
+                        author={post.authorName}
+                        image={post.mainImage && post.mainImage.asset && post.mainImage.asset.url}
+                        body={post.body} authorImage={post.authorImage && post.authorImage.asset && post.authorImage.asset.url}
+                        loading={loading}
+                        authors={authors.data}
+                        authorRef={post.author._ref}
+                    />
                 ))}
             </Wrap>
-
         </Container>
     );
 };
--- src/pages/SinglePost.js
+++ src/pages/SinglePost.js
@@ -1,6 +1,15 @@
+import BlockContent from "@sanity/block-content-to-react";
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import client from "../client";
+import {
+    Box,
+    Button,
+    Container,
+    Heading,
+    Image,
+    Text,
+} from "@chakra-ui/react";
 
 export default function SinglePost() {
     const [singlePost, setSinglePost] = useState([]);
@@ -21,26 +30,51 @@ export default function SinglePost() {
                 },
                 "authorName": author -> name,
             }`
-        ).then(data => setSinglePost(data[0])).catch(err => console.error(err));
-        setIsLoading(false);
+        ).then(data => { setSinglePost(data[0]); setIsLoading(false) }).catch(err => console.error(err));
     }, [slug]);
+
+    console.log(singlePost);
 
     return (
         <>
             {isLoading ? <h1>Loading...</h1> : (
-                <section>
-                    <h1>{singlePost.title}</h1>
-                    {singlePost.mainImage && singlePost.mainImage.asset &&
-                        <img src={singlePost.mainImage.asset.url} alt={singlePost.title} />
-                    }
-                    <p>By {singlePost.authorName}</p>
+                // <section>
+                //     <h1>{!isLoading && singlePost.title}</h1>
+                //     <img src={!isLoading && singlePost.mainImage.asset.url} alt={!isLoading && singlePost.title} />
+                //     <p>By {!isLoading && singlePost.authorName}</p>
+                //     <BlockContent blocks={!isLoading && singlePost.body} projectId="82l3omkv" dataset="production" />
 
-                    <button>
-                        <Link to="/blog">Read more articles</Link>
-                    </button>
-                </section>
-            )
-            }
+                //     <button>
+                //         <Link to="/blog">Read more articles</Link>
+                //     </button>
+                // </section>
+                <Container maxW={'8xl'} p="12">
+                    <Box p={5}>
+                        <Heading as="h1" color="#47A166" pl={6}>{!isLoading && singlePost.title}</Heading>
+                        <Box
+                            display="flex"
+                            marginTop={{ base: '1', sm: '5' }}
+                            mb="12"
+                        >
+                            <Image src={!isLoading ? singlePost.mainImage.asset.url : undefined} alt={!isLoading ? singlePost.title : "Blog post main cover"} width="250px" height="250px" />
+                            <Box
+                                display="flex"
+                                flexDirection="column"
+                                justifyContent="start"
+                                marginLeft="12"
+                            >
+                                <Text fontWeight="semibold" fontSize="2xl">By {!isLoading && singlePost.authorName}</Text>
+                                <BlockContent blocks={!isLoading && singlePost.body} projectId="82l3omkv" dataset="production" />
+                                <Link to="/blog">
+                                    <Button fontStyle="italic">
+                                        Read more articles
+                                    </Button>
+                                </Link>
+                            </Box>
+                        </Box>
+                    </Box>
+                </Container>
+            )}
         </>
-    )
+    );
 }
